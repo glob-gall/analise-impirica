421c681acd70cdca000e73405ea7a2b825cfa7fe
Vinicius Petris <vinicius.petris@b2wdigital.com>
2023-06-22 20:29:22 -0300

    add contador de variaveis no csv

--- dataset/403/Estrutura Condicional Encadeada/codigo-alunos/08/calcular_complexidade.py
+++ dataset/403/Estrutura Condicional Encadeada/codigo-alunos/08/calcular_complexidade.py
@@ -1,7 +1,15 @@
+import ast
 import csv
 import glob
 from radon.raw import analyze as raw_analyze
 from radon.complexity import cc_visit
+
+# Função para contar as variáveis em um nó da árvore sintática
+def contar_variaveis(node):
+    if isinstance(node, ast.Name) and not isinstance(node.ctx, ast.Load):  # Verifica se é uma atribuição de variável
+        return 1
+    else:
+        return sum(contar_variaveis(child) for child in ast.iter_child_nodes(node))
 
 # Obtém todos os arquivos .py no diretório atual
 file_list = glob.glob('*.py')
@@ -15,7 +23,7 @@ with open('metrics.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
 
     # Escreve o cabeçalho do arquivo CSV
-    writer.writerow(['Arquivo', 'LOC', 'LLOC', 'SLOC', 'Linhas de Comentário', 'Linhas em Branco', 'Complexidade Ciclomática'])
+    writer.writerow(['Arquivo', 'LOC', 'LLOC', 'SLOC', 'Linhas de Comentário', 'Linhas em Branco', 'Complexidade Ciclomática', 'Número de Variáveis'])
 
     for filename in file_list:
         try:
@@ -30,6 +38,10 @@ with open('metrics.csv', 'w', newline='') as csvfile:
             comment_lines = raw_metrics.comments
             blank_lines = raw_metrics.blank
 
+            # Contagem das variáveis
+            parsed_code = ast.parse(source_code)
+            num_variaveis = contar_variaveis(parsed_code)
+            
             # Cálculo da complexidade ciclomática
             # Corrige a indentação do código
             source_code = source_code.expandtabs()
@@ -40,12 +52,13 @@ with open('metrics.csv', 'w', newline='') as csvfile:
             results = cc_visit(code_wrapper, no_assert=True)
             total_complexity = sum(result.complexity for result in results)
 
+
             # Escreve os dados no arquivo CSV
-            writer.writerow([filename, loc, lloc, sloc, comment_lines, blank_lines, total_complexity])
+            writer.writerow([filename, loc, lloc, sloc, comment_lines, blank_lines, total_complexity, num_variaveis])
 
         except Exception as e:
             print(f"Erro ao processar o arquivo {filename}: {str(e)}")
-            writer.writerow([filename, 0, 0, 0, 0, 0, 0])
+            writer.writerow([filename, 0, 0, 0, 0, 0, 0, 0])
 
 print("Dados exportados para metrics.csv")
 
